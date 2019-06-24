db8209fed5b9695c20f56a1d17ba51866e28be4d
Mrigank Doshy <mrigankdoshy@gmail.com>
2019-06-23 23:50:00 -0400

    Update MonteCarloThreaded.c

--- src/Monte Carlo Simulation/Pi/Threaded/MonteCarloThreaded.c
+++ src/Monte Carlo Simulation/Pi/Threaded/MonteCarloThreaded.c
@@ -10,17 +10,17 @@
 A random number generator. 
 Guidance from from http://stackoverflow.com/a/3067387/1281089
 **/
-float randNumGen(){
+float randomNumberGenerator(){
 
-   int random_value = rand(); //Generate a random number   
-   float unit_random = random_value / (float) RAND_MAX; //make it between 0 and 1 
-   return unit_random;
+   int randomValue = rand(); //Generate a random number   
+   float randomUnit = randomValue / (float) RAND_MAX; //make it between 0 and 1 
+   return randomUnit;
 }
 
 /**
 The task allocated to a thread
 **/
-void *doCalcs(void *threadid)
+void *doit(void *threadid)
 {
    long longTid;
    longTid = (long)threadid;
@@ -29,48 +29,44 @@ void *doCalcs(void *threadid)
 
    //using malloc for the return variable in order make
    //sure that it is not destroyed once the thread call is finished
-   float *in_count = (float *)malloc(sizeof(float));
-   *in_count=0;
+   float *inCount = (float *)malloc(sizeof(float));
+   *inCount=0;
    
    //get the total number of iterations for a thread
-   float tot_iterations= TOT_COUNT/NUM_THREADS;
+   float totalIterations= TOT_COUNT/NUM_THREADS;
    
-   int counter=0;
+   int counter = 0;
    
    //calculation
-   for(counter=0;counter<tot_iterations;counter++){
-      float x = randNumGen();
-      float y = randNumGen();
+   for(counter = 0; counter < totalIterations; counter++) {
+      float x = randomNumberGenerator();
+      float y = randomNumberGenerator();
       
-      float result = sqrt((x*x) + (y*y));
+      float result = sqrt((x * x) + (y * y));
       
-      if(result<1){
-         *in_count+=1;         //check if the generated value is inside a unit circle
+      if(result < 1) {
+         *inCount+=1;         //check if the generated value is inside a unit circle
       }
       
    }
    
    //get the remaining iterations calculated by thread 0
-   if(tid==0){
-      float remainder = TOT_COUNT%NUM_THREADS;
+   if(tid == 0){
+      float remainder = TOT_COUNT % NUM_THREADS;
       
-      for(counter=0;counter<remainder;counter++){
-      float x = randNumGen();
-      float y = randNumGen();
+      for(counter = 0; counter < remainder; counter++) {
+      float x = randomNumberGenerator();
+      float y = randomNumberGenerator();
       
-      float result = sqrt((x*x) + (y*y));
+      float result = sqrt((x * x) + (y * y));
       
       if(result<1){
-         *in_count+=1;         //check if the generated value is inside a unit circle
+         *inCount+=1;         //check if the generated value is inside a unit circle
       }
-      
+     }
    }
-   }
-
-
-   //printf("In count from #%d : %f\n",tid,*in_count);
-   
-   pthread_exit((void *)in_count);     //return the in count
+   //printf("In count from #%d : %f\n",tid,*inCount);
+   pthread_exit((void *)inCount);     //return the in count
 }
 
 int main(int argc, char *argv[])
@@ -79,36 +75,35 @@ int main(int argc, char *argv[])
    int rc;
    long t;
    void *status;
-   float tot_in=0;
+   float total=0;
 
    // Calculate time taken by a request
   struct timespec requestStart, requestEnd;
   clock_gettime(CLOCK_REALTIME, &requestStart);
    
    for(t=0;t<NUM_THREADS;t++){
-     rc = pthread_create(&threads[t], NULL, doCalcs, (void *)t);
+     rc = pthread_create(&threads[t], NULL, doit, (void *)t);
      if (rc){
        printf("ERROR; return code from pthread_create() is %d\n", rc);
        exit(-1);
-       }
      }
+   }
 
    //join the threads
-   for(t=0;t<NUM_THREADS;t++){
+   for(t=0;t<NUM_THREADS;t++) {
            
       pthread_join(threads[t], &status);
       //printf("Return from thread %ld is : %f\n",t, *(float*)status);
       
-      tot_in+=*(float*)status;            //keep track of the total in count
-     
-     }
+      total+=*(float*)status;            //keep track of the total in count
+   }
      clock_gettime(CLOCK_REALTIME, &requestEnd);
      // Calculate time it took
-     double accum = ( requestEnd.tv_sec - requestStart.tv_sec )
-       + ( requestEnd.tv_nsec - requestStart.tv_nsec )
-       / BILLION;
-     printf( "Time taken: %lf\n", accum );
-     printf("Value for Pi: %f \n", 4*(tot_in/TOT_COUNT));
+     double timespent = ( requestEnd.tv_sec - requestStart.tv_sec )
+       + ( requestEnd.tv_nsec - requestStart.tv_nsec ) / BILLION;
+
+     printf("Time taken: %lf\n", timespent);
+     printf("Value for Pi: %f \n", 4*(total/TOT_COUNT));
    
    /* Last thing that main() should do */
    pthread_exit(NULL);
