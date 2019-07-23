792e4e81bd84e06eb70f1c9a0e5ef17e9fbe5b9b
Mrigank Doshy <mrigankdoshy@gmail.com>
2019-07-23 12:57:52 -0400

    Create cmake_clean.cmake

---
+++ src/Monte Carlo/MC/build/src/CMakeFiles/mc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mc.dir/gbm.cpp.o"
+  "CMakeFiles/mc.dir/heston.cpp.o"
+  "libmc.dylib"
+  "libmc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
