2aae3bc8ac134ff61d2bbe97f99a9d0356ff4091
Mrigank Doshy <mrigankdoshy@gmail.com>
2019-07-23 13:06:59 -0400

    Create cmake_clean.cmake

---
+++ src/Monte Carlo/MC/build/tests/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/interface.cpp.o"
+  "CMakeFiles/example.dir/plot.cpp.o"
+  "CMakeFiles/example.dir/print.cpp.o"
+  "example"
+  "example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
